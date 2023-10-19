2fd435ab5d65dcbf42632b0356acbdc565511501
kimery9 <114025961+kimery9@users.noreply.github.com>
2023-10-19 16:24:17 -0400

    mm-app

---
+++ Museum_Mate/App.js
@@ -0,0 +1,51 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import BarcodeScanner from './src/BarcodeScanner';
+import HomeScreen from './src/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+          headerStyle: {
+            backgroundColor: '#7574DA', // Set the background color of the header
+          },
+          headerTintColor: '#fff', // Set the text color of the header
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+      <Stack.Screen
+        name="Scanner"
+        component={BarcodeScanner}
+        options={{
+          title: 'Scanner',
+          headerStyle: {
+            backgroundColor: '#7574DA',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+}
